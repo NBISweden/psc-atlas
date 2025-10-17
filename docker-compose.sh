7403003e0031560a834d04c4a5f8791b769df9f5
Andreas Kähäri <andreas.kahari@nbis.se>
2025-10-17 08:54:18 +0200

    feat: Make the "docker-compose.sh" script pass UID and GID to build process

--- docker-compose.sh
+++ docker-compose.sh
@@ -1,6 +1,11 @@
 #!/bin/sh -
 
 # This script acts as a wrapper for the "docker compose" command when
-# working in the development environment.
+# working in the development environment.  It also passes the UID and
+# GID of the current user into the environment.
 
-exec docker compose -f docker-compose.yml -f docker-compose.dev.yml "$@"
+exec env UID="$(id -u)" GID="$(id -g)" \
+	docker compose \
+		-f docker-compose.yml \
+		-f docker-compose.dev.yml \
+		"$@"
