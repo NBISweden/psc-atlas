8dd3a0b99c08ca66c46bdac5d1b992c4601d79e8
Henrike <henrike.wiemker@t-online.de>
2025-10-29 16:01:12 +0100

    feat: add custom select & checkbox styles

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -12,6 +12,23 @@ export default function Home() {
       <button type="button" className="btn btn-outline-accent">
         Outline test
       </button>
+      <div className="form-check">
+        <input
+          className="form-check-input p-2"
+          type="checkbox"
+          value=""
+          id="flexCheckDefault"
+        />
+        <label className="form-check-label" htmlFor="flexCheckDefault">
+          Default checkbox
+        </label>
+      </div>
+      <select className="form-select" aria-label="Default select example">
+        <option selected>Open this select menu</option>
+        <option value="1">One</option>
+        <option value="2">Two</option>
+        <option value="3">Three</option>
+      </select>
     </section>
   );
 }
