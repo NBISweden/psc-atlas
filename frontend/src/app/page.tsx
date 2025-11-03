4d4246fcbc89c381936e42498c3d0741bd56a954
Henrike <henrike.wiemker@t-online.de>
2025-11-03 14:13:09 +0100

    fix: use defaultValue instead of selected

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -20,8 +20,12 @@ export default function Home() {
           Default checkbox
         </label>
       </div>
-      <select className="form-select" aria-label="Default select example">
-        <option selected>Open this select menu</option>
+      <select
+        className="form-select"
+        defaultValue="0"
+        aria-label="Default select example"
+      >
+        <option value="0">Open this select menu</option>
         <option value="1">One</option>
         <option value="2">Two</option>
         <option value="3">Three</option>
