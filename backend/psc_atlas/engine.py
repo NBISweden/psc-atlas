0fa3812ceae4df9502785603c33fc5e2ecd2861d
Andreas Kähäri <andreas.kahari@nbis.se>
2025-11-05 09:20:45 +0100

    chore: Turn off debugging

--- backend/psc_atlas/engine.py
+++ backend/psc_atlas/engine.py
@@ -3,6 +3,6 @@ from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "sqlite:///./database.db"
 
-engine = create_engine(DATABASE_URL, echo=True)
+engine = create_engine(DATABASE_URL, echo=False)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
