bd21df410b93b740eff8dbcd6aaddb8461f750de
Henrike <henrike.wiemker@t-online.de>
2025-11-05 14:01:40 +0100

    feat: add dummy violin plot

--- frontend/src/app/explore-data/page.tsx
+++ frontend/src/app/explore-data/page.tsx
@@ -5,32 +5,62 @@ import Plot from "./../components/Plot/Plot";
 import exampleData from "./../example-data.json";
 
 const ExploreData: React.FC = () => {
-  const [boxClicked, setBoxClicked] = useState<boolean>(false);
+  const [scatterClicked, setScatterClicked] = useState<boolean>(false);
+  const [violinClicked, setViolinClicked] = useState<boolean>(false);
 
   return (
-    <section>
-      <div>
-        <Plot
-          data={[
-            {
-              x: exampleData.x,
-              y: exampleData.y,
-              type: "scatter",
-              mode: "markers",
-              marker: { color: "f56a6a" },
-            },
-          ]}
-          layout={{ width: 700, height: 500 }}
-          onClick={() => setBoxClicked(true)}
-        />
-      </div>
-      {boxClicked && (
+    <>
+      <section>
         <div>
-          <p>The plot has been clicked!</p>
-          <button onClick={() => setBoxClicked(false)}>Hide me again</button>
+          <Plot
+            data={[
+              {
+                x: exampleData.x,
+                y: exampleData.y,
+                type: "scatter",
+                mode: "markers",
+                marker: { color: "f56a6a" },
+              },
+            ]}
+            layout={{ width: 700, height: 500 }}
+            onClick={() => setScatterClicked(true)}
+          />
         </div>
-      )}
-    </section>
+        {scatterClicked && (
+          <div>
+            <p>The plot has been clicked!</p>
+            <button onClick={() => setScatterClicked(false)}>
+              Hide me again
+            </button>
+          </div>
+        )}
+      </section>
+      <section>
+        <div>
+          <Plot
+            data={[
+              {
+                x: exampleData.category,
+                y: exampleData.score,
+                type: "violin",
+                mode: "markers",
+                marker: { color: "f56a6a" },
+              },
+            ]}
+            layout={{ width: 700, height: 500 }}
+            onClick={() => setViolinClicked(true)}
+          />
+        </div>
+        {violinClicked && (
+          <div>
+            <p>The plot has been clicked!</p>
+            <button onClick={() => setViolinClicked(false)}>
+              Hide me again
+            </button>
+          </div>
+        )}
+      </section>
+    </>
   );
 };
 
