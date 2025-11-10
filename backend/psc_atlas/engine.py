1d3fee5ed45c61ddce3e60dcbfd21440de4d0536
Andreas Kähäri <andreas.kahari@nbis.se>
2025-11-10 17:06:35 +0100

    feat: Get database path from environment variable

--- backend/psc_atlas/engine.py
+++ backend/psc_atlas/engine.py
@@ -1,7 +1,8 @@
+from os import getenv
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "sqlite:///./database.db"
+DATABASE_URL = getenv("DATABASE_URL", "sqlite:///vol/database/psc-atlas.db")
 
 engine = create_engine(DATABASE_URL, echo=False)
 
