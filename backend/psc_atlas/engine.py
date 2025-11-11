8e9021b0777ddd655f4be0799ae68cc90a74222a
Andreas Kähäri <andreas.kahari@nbis.se>
2025-11-11 14:00:11 +0100

    fix: Pick up the database in the "$HOME/vol/database/" directory

--- backend/psc_atlas/engine.py
+++ backend/psc_atlas/engine.py
@@ -1,8 +1,16 @@
 from os import getenv
+from pathlib import Path
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = getenv("DATABASE_URL", "sqlite:///vol/database/psc-atlas.db")
+# The default database URL can be overridden by setting the DATABASE_URL
+# environment variable.  The default uses a SQLite database located at
+# "$HOME/vol/database/psc-atlas.db".
+DATABASE_URL = getenv(
+    "DATABASE_URL",
+    f"sqlite:///{Path.home()}/vol/database/psc-atlas.db",
+)
 
 engine = create_engine(DATABASE_URL, echo=False)
 
