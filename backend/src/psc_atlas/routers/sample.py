7477fe494d0ab5bafbfb8d791cf5fe8097a5e13b
Andreas Kähäri <andreas.kahari@nbis.se>
2025-11-17 22:11:01 +0100

    feat: Add conditions and their valid values

--- backend/src/psc_atlas/routers/sample.py
+++ backend/src/psc_atlas/routers/sample.py
@@ -6,13 +6,26 @@ from typing import List
 from sqlalchemy import distinct
 
 from psc_atlas.session import get_session
-from psc_atlas.models import Sample
+from psc_atlas.models import Sample, YesNo, HiLo
 
 router = APIRouter()
 
 
+class Condition(BaseModel):
+    name: str
+    values: List[YesNo | HiLo | None]
+
+
 class SampleTypeResponse(BaseModel):
     types: List[str]
+    conditions: List[Condition] = [
+        {"name": "psc", "values": [YesNo.YES, YesNo.NO, None]},
+        {"name": "cca", "values": [YesNo.YES, YesNo.NO, None]},
+        {"name": "ibd", "values": [YesNo.YES, YesNo.NO, None]},
+        {"name": "fibrosis", "values": [HiLo.HIGH, HiLo.LOW, None]},
+        {"name": "bilirubin", "values": [HiLo.HIGH, HiLo.LOW, None]},
+        {"name": "alp", "values": [HiLo.HIGH, HiLo.LOW, None]},
+    ]
 
 
 @router.get("/types")
@@ -25,7 +38,7 @@ def get_sample_types() -> SampleTypeResponse:
 
     Returns:
         SampleTypeResponse: A response model containing a list of
-        distinct sample types.
+        distinct sample types and the predefined conditions.
     """
 
     sample_types: list[str] = []
