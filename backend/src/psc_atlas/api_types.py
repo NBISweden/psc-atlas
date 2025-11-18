a4923b3ca3e11183ca1cfbba2772e154b6aada30
Andreas Kähäri <andreas.kahari@nbis.se>
2025-11-18 12:34:00 +0100

    chore: Not needed after all

--- backend/src/psc_atlas/api_types.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel
-from typing import List, Optional
-
-from psc_atlas.models import Sample, YesNo, HiLo
-
-
-class Condition(BaseModel):
-    name: str
-    values: List[Optional[YesNo | HiLo]]
