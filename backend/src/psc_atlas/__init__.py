1c80f3c35d27535ae02fe7c7c48b3d49aaef55b7
Andreas Kähäri <andreas.kahari@nbis.se>
2025-11-26 10:24:12 +0100

    feat: All endpoints are now sample endpoints

--- backend/src/psc_atlas/__init__.py
+++ backend/src/psc_atlas/__init__.py
@@ -1,12 +1,10 @@
 from psc_atlas.routers import core
 from psc_atlas.routers import sample
-from psc_atlas.routers import variable
-from psc_atlas.routers import measurement
 from fastapi import FastAPI
 import os
 
 
-DEFAULT_ROOT_PATH = os.environ.get("PSC_ATLAS_DEFAULT_ROOT_PATH", "")
+DEFAULT_ROOT_PATH = os.environ.get("PSC_ATLAS_DEFAULT_ROOT_PATH", "/api")
 
 
 def create_app(root_path=DEFAULT_ROOT_PATH):
@@ -18,13 +16,5 @@ def create_app(root_path=DEFAULT_ROOT_PATH):
     app.include_router(
         sample.router,
         prefix="/v1/sample",
-    )
-    app.include_router(
-        variable.router,
-        prefix="/v1/variable",
-    )
-    app.include_router(
-        measurement.router,
-        prefix="/v1/measurement",
     )
     return app
