4aa6537a0cfcbd74b28cf8d1880a17856dbdfdfc
Andreas Kähäri <andreas.kahari@nbis.se>
2025-12-04 13:27:24 +0100

    fix: It's even simpler than that
    No need for explicit close() or for "try" at all.

--- backend/src/psc_atlas/session.py
+++ backend/src/psc_atlas/session.py
@@ -10,12 +10,5 @@ logger = logging.getLogger(__name__)
 @contextmanager
 def get_session():
     """Provide a transactional scope around a series of operations."""
-    session = SessionLocal()
-    try:
-        with session.begin():
-            yield session
-    except Exception as e:
-        logger.exception("Session rolled back due to an exception: %s", e)
-        raise
-    finally:
-        session.close()
+    with SessionLocal() as session, session.begin():
+        yield session
