752f7104a0933f4c40f3400963c1da837e229a47
Henrike <henrike.wiemker@t-online.de>
2025-12-12 16:16:07 +0100

    feat: remove dummy content from home page. Issue #60

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -6,40 +6,7 @@ const Home = () => {
   return (
     <>
       <section>
-        <h2>PSC Atlas</h2>
-        <p>Hello world!</p>
-        <button type="button" className="btn btn-primary">
-          Button test
-        </button>
-        <button type="button" className="btn btn-outline-primary">
-          Outline test
-        </button>
-        <div className="form-check">
-          <input
-            className="form-check-input p-2"
-            type="checkbox"
-            value=""
-            id="flexCheckDefault"
-          />
-          <label className="form-check-label" htmlFor="flexCheckDefault">
-            Default checkbox
-          </label>
-        </div>
-        <select
-          className="form-select"
-          defaultValue="0"
-          aria-label="Default select example"
-        >
-          <option value="0">Open this select menu</option>
-          <option value="1">One</option>
-          <option value="2">Two</option>
-          <option value="3">Three</option>
-        </select>
-      </section>
-      <section>
-        <nav>
-          <Link href="/explore-data">Explore Data</Link>
-        </nav>
+        <h2>Welcome to the SUPRIM Atlas!</h2>
       </section>
     </>
   );
